9becd6ac1a16cd28ded61838cc3657a6a1a11569
Oleg Tsvetkov <olecve@gmail.com>
2023-08-31 23:03:32 +0300

    Add address validation, setColumn, setRow

---
+++ src/index.spec.ts
@@ -0,0 +1,37 @@
+import { isValidCellAddress, isValidCellRange, setColumn, setRow } from './index'
+
+it('isValidCellAddress', () => {
+  expect(isValidCellAddress('A1')).toBe(true)
+  expect(isValidCellAddress('A2')).toBe(true)
+  expect(isValidCellAddress('Z1')).toBe(true)
+  expect(isValidCellAddress('Z2')).toBe(true)
+  expect(isValidCellAddress('AA11')).toBe(true)
+  expect(isValidCellAddress('AB12')).toBe(true)
+  expect(isValidCellAddress('ABC123')).toBe(true)
+
+  expect(isValidCellAddress('A0')).toBe(false)
+  expect(isValidCellAddress('A')).toBe(false)
+  expect(isValidCellAddress('Z')).toBe(false)
+  expect(isValidCellAddress('0')).toBe(false)
+  expect(isValidCellAddress('1')).toBe(false)
+})
+
+it('isValidCellRange', () => {
+  expect(isValidCellRange('A1:B2')).toBe(true)
+  expect(isValidCellRange('AA10:AB20')).toBe(true)
+
+  expect(isValidCellRange('A1:B')).toBe(false)
+  expect(isValidCellRange('A:B1')).toBe(false)
+  expect(isValidCellRange('A1:2')).toBe(false)
+  expect(isValidCellRange('2:B2')).toBe(false)
+})
+
+it('setRow', () => {
+  expect(setRow('A1', 99)).toBe('A99')
+  expect(setRow('A1', '99')).toBe('A99')
+})
+
+it('setColumn', () => {
+  expect(setColumn('A1', 'B')).toBe('B1')
+  expect(setColumn('A1', 'AZ')).toBe('AZ1')
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
- 
+const cellAddressPattern = /^[A-Z]+[1-9]\d*$/
+const cellRangePattern = /^[A-Z]+[1-9]\d*:[A-Z]+[1-9]\d*$/
+
+export function isValidCellAddress(address: string): boolean {
+  return cellAddressPattern.test(address)
+}
+
+export function isValidCellRange(range: string): boolean {
+  return cellRangePattern.test(range)
+}
+
+export function setColumn(address: string, newColumn: number | string) {
+  const rowMatch = address.match(/\d+/)
+  if (!rowMatch) throw new Error(`Invalid address format: ${address}`)
+  const rowPart = rowMatch[0]
+  return newColumn + rowPart
+}
+
+export function setRow(address: string, newRow: number | string) {
+  const columnMatch = address.match(/[A-Z]+/)
+  if (!columnMatch) throw new Error(`Invalid address format: ${address}`)
+  const columnPart = columnMatch[0]
+  return columnPart + newRow
+}
